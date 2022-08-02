142ffaf73871a5b566d31891b86be18556a2ee25
Stanislav <lazarev@yandex.ru>
2022-08-02 17:55:37 +0300

    fix google auth and button theme

--- client/src/components/Forms/GoogleAuth/GoogleAuth.jsx
+++ client/src/components/Forms/GoogleAuth/GoogleAuth.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getGoogleDataThunk } from '../../../redux/actions/googleAuthActions';
+/* global google */
 
 export default function GoogleAuth() {
   const dispatch = useDispatch();
@@ -11,7 +12,6 @@ export default function GoogleAuth() {
   }
 
   useEffect(() => {
-    /* global google */
     google.accounts.id.initialize({
       client_id: '841875808140-265h78c8ini3d4da85k7g2hu3cphc7uu.apps.googleusercontent.com',
       callback: handleCallbackResponse,
