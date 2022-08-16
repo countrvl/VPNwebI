41c3ff04f5298f3dfe96cf442d11b78caef1b5e7
metrgram118 <ihctuyd@mail.ru>
2022-08-16 19:51:40 +0300

    release 2 dep

--- server/db/database.js
+++ server/db/database.js
@@ -1,17 +1,9 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const {
-  DB_USERNAME, DB_USER_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT,
-} = process.env;
-
-module.exports = {
-  development: {
-    username: DB_USERNAME,
-    password: DB_USER_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
-    seederStorage: 'sequelize',
-    seederStorageTableName: 'SequelizeData',
-  },
-};
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
